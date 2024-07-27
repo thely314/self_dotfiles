a62cb90e0fe676f26d95dbc3f638445dfb072fa8
lone <1715849169@qq.com>
2024-07-27 17:32:52 +0800

    2024/07/27 updated setup and .zshrc

--- setupDotFile.sh
+++ setupDotFile.sh
@@ -1,7 +1,35 @@
 #!/bin/bash
 
-DotFileDir="~/dotfile"
+DotDIR="$PWD"
 
-ln -s "$DotFileDir/.bashrc" "$HOME/.bashrc"
+echo "Suggestion: place this dotfile directory in '$HOME'"
+
+#backup file if old one exist
+backup() {
+	target=$1
+	if [ -e "$target" ]; then
+			if [ ! -L "$target" ]; then
+					mv "$target" "$target.backup"
+					echo "--- Backup your old '$target' file to '$target.backup'"
+			fi
+	fi
+}
+
+symlinkCreate() {
+	file=$1
+	link=$2
+	if [ ! -e "$link" ]; then
+			echo "--- Symlinking your new '$link'"
+			ln -s $file $link
+	fi
+}
+
+for name in .bashrc; do
+		if [ ! -d "$name" ]; then
+				target="$HOME/$name"
+				backup $target
+				symlinkCreate $DotDIR/$name $target
+		fi
+done
 
 echo "DotFiles symlinked successfully!"
